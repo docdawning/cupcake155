eeb55019c0c15f0cb0202ea225cb1e95baafe90b
docdawning <doc@dawning.ca>
2016-01-24 13:50:13 -0700

    Updated scripts to be a little more relative

--- Cupcake155-RAMBo/Marlin/copyLatestFiles.sh
+++ Cupcake155-RAMBo/Marlin/copyLatestFiles.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Copies local Config files in to Marlin
 
-MARLIN=/Users/j/ownCloud/Projects/Printing/Cupcake155/Firmware/Marlin/Marlin-1.0.2-1/Marlin/
+#PWD=/opt/Cupcake155/docdawning_cupcake155/Cupcake155-RAMBo/Marlin
+#MARLIN_PI=/opt/Cupcake155/Marlin-1.0.2-1/Marlin/
+#MARLIN_OSX=/Users/j/ownCloud/Projects/Printing/Cupcake155/Firmware/Marlin/Marlin-1.0.2-1/Marlin/
 DRY='cp '
-SRC=$MARLIN
+SRC=../../../Marlin-1.0.2-1/Marlin/
 $DRY $SRC/Configuration.h .
 $DRY $SRC/Configuration_adv.h .
 $DRY $SRC/pins.h .
--- Cupcake155-RAMBo/Marlin/putLatestFilesInMarlin.sh
+++ Cupcake155-RAMBo/Marlin/putLatestFilesInMarlin.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Copies local Config files in to Marlin
 
-MARLIN=/Users/j/ownCloud/Projects/Printing/Cupcake155/Firmware/Marlin/Marlin-1.0.2-1/Marlin/
+#PWD=/opt/Cupcake155/docdawning_cupcake155/Cupcake155-RAMBo/Marlin
+#MARLIN_PI=/opt/Cupcake155/Marlin-1.0.2-1/Marlin/
+#MARLIN_OSX=/Users/j/ownCloud/Projects/Printing/Cupcake155/Firmware/Marlin/Marlin-1.0.2-1/Marlin/
 #HERE
 DRY='cp '
-DEST=$MARLIN
+DEST=../../../Marlin-1.0.2-1/Marlin/
 $DRY Configuration.h $DEST
 $DRY Configuration_adv.h $DEST
 $DRY pins.h $DEST
