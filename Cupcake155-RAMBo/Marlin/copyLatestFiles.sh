c7c3b8df45d61f984ce5fc62d6564430714ce314
DocDawning <doc@dawning.ca>
2016-01-24 13:30:25 -0700

    Updated helper scripts

--- Cupcake155-RAMBo/Marlin/copyLatestFiles.sh
+++ Cupcake155-RAMBo/Marlin/copyLatestFiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-DRY='cp /home/dev/Cupcake155/Marlin-1.0.2-1/Marlin'
-$DRY/Configuration.h .
-$DRY/Configuration_adv.h .
-$DRY/pins.h .
+# Copies local Config files in to Marlin
+
+MARLIN=/Users/j/ownCloud/Projects/Printing/Cupcake155/Firmware/Marlin/Marlin-1.0.2-1/Marlin/
+DRY='cp '
+SRC=$MARLIN
+$DRY $SRC/Configuration.h .
+$DRY $SRC/Configuration_adv.h .
+$DRY $SRC/pins.h .
