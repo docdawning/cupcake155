d443647aaab1687943e451aef7fbbf6668e26f30
DocDawning <doc@dawning.ca>
2016-02-20 10:11:34 -0700

    Defined V0.6. Reduced Z motor current. Reduced cooling fan speed

--- Cupcake155-RAMBo/Marlin/Configuration.h
+++ Cupcake155-RAMBo/Marlin/Configuration.h
@@ -25,7 +25,7 @@
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "DocDawning, Cupcake155 [RAMBo]" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "DocDawning, Cupcake155 Configuration V0.6" // Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.
@@ -296,7 +296,7 @@
 // The pullups are needed if you directly connect a mechanical endswitch between the signal and ground pins.
 const bool X_MIN_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
 const bool Y_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
-const bool Z_MIN_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
+const bool Z_MIN_ENDSTOP_INVERTING = false; // set to true to invert the logic of the endstop.
 const bool X_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
 const bool Y_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
 const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of the endstop.
@@ -504,8 +504,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 //Cupcake155 (Rambo)
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {93.727842795, 93.727842795, 2548.8, 101.0869}  // default steps per unit for Cupcake155 (on RAMBo)
-#define DEFAULT_MAX_FEEDRATE          {3000, 3000, 2, 5000/60}    // (mm/sec)    
-#define DEFAULT_MAX_ACCELERATION      {2000,2000,10,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_MAX_FEEDRATE          {2000, 2000, 2, 5000/60}    // (mm/sec)    
+#define DEFAULT_MAX_ACCELERATION      {750,750,10,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          2000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves 
 #define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for r retracts
